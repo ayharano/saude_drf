f342a7c066527124c5263bcec9c6a26580eed189
Alexandre Harano <email@ayharano.dev>
2023-12-08 03:28:15 -0300

    chore: set DRF settings to use JSON as default content type

--- src/saude_drf/settings.py
+++ src/saude_drf/settings.py
@@ -54,6 +54,15 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+    ],
+}
+
 ROOT_URLCONF = 'saude_drf.urls'
 
 TEMPLATES = [
