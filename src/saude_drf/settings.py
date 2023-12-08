4ca7c76435729ea6ca54d785b1e2d4621b500af4
Alexandre Harano <email@ayharano.dev>
2023-12-08 03:23:27 -0300

    chore: enable django-filters as the default DRF filtering backend

--- src/saude_drf/settings.py
+++ src/saude_drf/settings.py
@@ -65,6 +65,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': [
         'rest_framework.parsers.JSONParser',
     ],
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
 }
 
 ROOT_URLCONF = 'saude_drf.urls'
